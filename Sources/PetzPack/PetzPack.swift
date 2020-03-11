b79a862c6da5270def31de5c8335e7941ff06afd
Cardozo <caiocardozocb3@gmail.com>
2020-03-11 15:33:22 -0300

    test 2

--- Sources/PetzPack/PetzPack.swift
+++ Sources/PetzPack/PetzPack.swift
@@ -2,7 +2,7 @@ struct PetzPack {
     var text = "Hello, World!"
 }
 
-struct Calculator {
+public struct Calculator {
     public init() {}
     
     public static func multply(_ valueX: Int, _ ValueY: Int) -> Int {
