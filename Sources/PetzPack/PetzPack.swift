ef852647aac2717fd3f8374a1919909d0c6ab357
Cardozo <caiocardozocb3@gmail.com>
2020-03-11 14:08:25 -0300

    Add Calculator

--- Sources/PetzPack/PetzPack.swift
+++ Sources/PetzPack/PetzPack.swift
@@ -1,3 +1,11 @@
 struct PetzPack {
     var text = "Hello, World!"
 }
+
+public class Claculator {
+    public init()
+    
+    public func multply(_ valueX: Int, _ ValueY: Int) -> Int {
+        return valueX * ValueY
+    }
+}
