dcb5647a79c58485980d889134ec183acc4984fe
Cardozo <caiocardozocb3@gmail.com>
2020-03-12 07:30:42 -0300

    include DSSettings

--- Sources/PetzPack/DSSystem.swift
+++ Sources/PetzPack/DSSystem.swift
@@ -7,16 +7,16 @@
 
 import UIKit
 
-public typealias PetzPackRoundedCorner = CGFloat
+public typealias PetzPackDSSettings = CGFloat
 
-public extension PetzPackRoundedCorner {
-//static let opaque: PetzPackLayoutConstant = 4
+public extension PetzPackDSSettings {
     
+    // MARK: - Opacity
     enum Opacity: CGFloat {
-            case opaque = 0.72
-            case medium = 0.50
-            case light = 0.24
-        }
+        case opaque = 0.72
+        case medium = 0.50
+        case light = 0.24
+    }
     
     // MARK: - Corner
     enum RoundedCorner: CGFloat {
@@ -24,16 +24,7 @@ public extension PetzPackRoundedCorner {
         case small = 4
         case medium = 8
         case big = 16
-        // pill = height/2
     }
-}
-
-
-public class PetzPackDS {
-    // MARK: - Opacity
-   
-    
-    
     
     // MARK: - Border
     enum BorderWidth: CGFloat {
@@ -41,5 +32,4 @@ public class PetzPackDS {
         case medium = 2
         case big = 4
     }
-    
 }
